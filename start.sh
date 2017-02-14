df7f3c881f17ee79ddaf155840a70f9ade535dea
txazo <784990655@qq.com>
2017-02-14 16:26:47 +0800

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-./bin/startup.sh
+function stop() {
+    pid='0'`ps -ef | grep tomcat | grep org.apache.catalina.startup.Bootstrap | awk '{print $2}'`
+    [ $pid -ne 0 ] && kill -9 $pid
+}
+
+function start() {
+    ./bin/startup.sh
+}
+
+stop
+start
